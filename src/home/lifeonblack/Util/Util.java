01c30be8000481c49cbfe2e3984db97386112cbf
Raymart <raymart.sarmiento23@gmail.com>
2017-06-21 00:12:54 -0700

    Created

--- src/home/lifeonblack/Util/Util.java
+++ src/home/lifeonblack/Util/Util.java
@@ -1,7 +1,17 @@
 package home.lifeonblack.Util;
 
 import home.lifeonblack.files.Language;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Raymart on 6/20/2017.
@@ -20,5 +30,47 @@ public class Util {
 
     public static String getLanguage() {
         return "lang-" + language.toLowerCase();
+    }
+
+    public static void saveHome(Location location, Player player, Plugin plugin, String homename) {
+        String world = location.getWorld().getName();
+        double x = location.getX();
+        double y = location.getY();
+        double z = location.getZ();
+        double yaw = location.getYaw();
+        double pitch = location.getPitch();
+        String name = player.getName();
+
+        plugin.getDataFolder().mkdir();
+        File file = new File(plugin.getDataFolder() + "/homes", name + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        config.set(homename + ".world", world);
+        config.set(homename + ".x", x);
+        config.set(homename + ".y", y);
+        config.set(homename + ".z", z);
+        config.set(homename + ".yaw", yaw);
+        config.set(homename + ".pitch", pitch);
+        try {
+            config.save(file);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Location getHome(Player player, Plugin plugin, String homename) {
+        String name = player.getName();
+        File file = new File(plugin.getDataFolder() + "/homes", name + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        if(!file.exists()) {
+            return null;
+        }
+        World world = Bukkit.getWorld(config.getString(homename + ".world"));
+        double x = config.getDouble(homename + ".x");
+        double y = config.getDouble(homename + ".y");
+        double z = config.getDouble(homename + ".z");
+        double yaw = config.getDouble(homename + ".yaw");
+        double pitch = config.getDouble(homename + ".pitch");
+        Location location = new Location(world, x, y, z, (float) yaw, (float)pitch);
+        return location;
     }
 }
